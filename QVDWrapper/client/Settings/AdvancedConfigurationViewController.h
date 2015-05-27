49f40d82fdcb40db01324aafc00b95208ba1ee6d
Oscar Costoya Vidal <oscarcv@gmail.com>
2015-05-27 10:05:35 +0200

    Fixes iPhone application icon required

---
+++ QVDWrapper/client/Settings/AdvancedConfigurationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AdvancedConfigurationViewController.h
+//  client
+//
+//  Created by Oscar Costoya Vidal on 27/5/15.
+//  Copyright (c) 2015 Qindel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdvancedConfigurationViewController : UIViewController
+
+@end
